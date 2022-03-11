a83cadedc20f5b6287e39942d0143cb54fb7e40f
smitchhatrola2505 <schhatrola779@rku.ac.in>
2022-03-11 14:43:16 +0530

    First Commit

---
+++ Export.js
@@ -0,0 +1,27 @@
+// * When Pass Only One Function
+
+// const Fun = (a,b) =>{
+//  return a+b;
+// };
+// module.exports = Fun;
+
+// * When Pass Multiple Values
+
+// const add = (a,b)=>{
+//   return a+b;
+// };
+
+// const sub = (a,b) =>{
+//   return a-b;
+// };
+
+// const mult = (a,b)=>{
+//     return a*b;
+// };
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+    // OR
+    
+// module.exports={add,sub,mult};
---
+++ Import.js
@@ -0,0 +1,17 @@
+// * when you pass import one function
+
+// const add = require("./Export.js");
+// console.log(add(2,5));
+
+// * when you import multiple function
+
+// const oper = require('./Export');
+// console.log(oper.add(5,4));
+// console.log(oper.sub(5,1));
+ 
+    // OR
+ 
+// const {add,sub,mult}=require('./Export');
+// console.log(add(5,4));
+// console.log(sub(10,1));
+// console.log(mult(2,5));
